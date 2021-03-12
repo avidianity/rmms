cf8c74188720172dcd28879145814cdaecdfcb5a
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2021-03-12 21:29:33 +0800

    Update

--- frontend/src/Components/Dashboard/Stats/Counts.tsx
+++ frontend/src/Components/Dashboard/Stats/Counts.tsx
@@ -1,18 +1,23 @@
 import React, { FC } from 'react';
 import { Counts as CountsContract } from '../../../Contracts/misc';
+import { User } from '../../../Contracts/User';
+import { outIf } from '../../../helpers';
+import state from '../../../state';
 
 type Props = {
 	counts: CountsContract;
 };
 
 const Counts: FC<Props> = ({ counts }) => {
+	const user = state.get<User>('user');
+
 	return (
 		<div className='row'>
 			<div className='col-12 pb-3'>
 				<h3>Totals</h3>
 			</div>
-			<div className='col-lg-3 col-md-4 col-sm-12'>
-				<div className='card card-stats'>
+			<div className={`col-md-4 col-sm-12 ${outIf(!['Pharmacist', 'Admin'].includes(user.role), 'd-none')}`}>
+				<div className={`card card-stats`}>
 					<div className='card-header card-header-warning card-header-icon'>
 						<div className='card-icon'>
 							<i className='material-icons'>medication</i>
@@ -28,55 +33,7 @@ const Counts: FC<Props> = ({ counts }) => {
 					</div>
 				</div>
 			</div>
-			<div className='col-lg-3 col-md-4 col-sm-12'>
-				<div className='card card-stats'>
-					<div className='card-header card-header-success card-header-icon'>
-						<div className='card-icon'>
-							<i className='material-icons'>supervised_user_circle</i>
-						</div>
-						<p className='card-category'>Patients</p>
-						<h4 className='card-title'>{counts.patients}</h4>
-					</div>
-					<div className='card-footer'>
-						<div className='stats'>
-							<i className='material-icons'>filter_alt</i> Total
-						</div>
-					</div>
-				</div>
-			</div>
-			<div className='col-lg-3 col-md-4 col-sm-12'>
-				<div className='card card-stats'>
-					<div className='card-header card-header-danger card-header-icon'>
-						<div className='card-icon'>
-							<i className='material-icons'>pregnant_woman</i>
-						</div>
-						<p className='card-category'>Prenatal Records</p>
-						<h4 className='card-title'>{counts.prenatal_records}</h4>
-					</div>
-					<div className='card-footer'>
-						<div className='stats'>
-							<i className='material-icons'>local_offer</i> Total
-						</div>
-					</div>
-				</div>
-			</div>
-			<div className='col-lg-3 col-md-4 col-sm-12'>
-				<div className='card card-stats'>
-					<div className='card-header card-header-info card-header-icon'>
-						<div className='card-icon'>
-							<i className='material-icons'>contact_page</i>
-						</div>
-						<p className='card-category'>Regular Records</p>
-						<h4 className='card-title'>{counts.regular_records}</h4>
-					</div>
-					<div className='card-footer'>
-						<div className='stats'>
-							<i className='material-icons'>update</i> Total
-						</div>
-					</div>
-				</div>
-			</div>
-			<div className='col-md-4 col-sm-12'>
+			<div className={`col-md-4 col-sm-12 ${outIf(!['Pharmacist', 'Admin'].includes(user.role), 'd-none')}`}>
 				<div className='card card-stats'>
 					<div className='card-header card-header-primary card-header-icon'>
 						<div className='card-icon'>
@@ -92,7 +49,7 @@ const Counts: FC<Props> = ({ counts }) => {
 					</div>
 				</div>
 			</div>
-			<div className='col-md-4 col-sm-12'>
+			<div className={`col-md-4 col-sm-12 ${outIf(!['Pharmacist', 'Admin'].includes(user.role), 'd-none')}`}>
 				<div className='card card-stats'>
 					<div className='card-header card-header-danger card-header-icon'>
 						<div className='card-icon'>
@@ -108,7 +65,55 @@ const Counts: FC<Props> = ({ counts }) => {
 					</div>
 				</div>
 			</div>
-			<div className='col-md-4 col-sm-12'>
+			<div className='col-lg-3 col-md-6 col-sm-12'>
+				<div className='card card-stats'>
+					<div className='card-header card-header-success card-header-icon'>
+						<div className='card-icon'>
+							<i className='material-icons'>supervised_user_circle</i>
+						</div>
+						<p className='card-category'>Patients</p>
+						<h4 className='card-title'>{counts.patients}</h4>
+					</div>
+					<div className='card-footer'>
+						<div className='stats'>
+							<i className='material-icons'>filter_alt</i> Total
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className='col-lg-3 col-md-6 col-sm-12'>
+				<div className='card card-stats'>
+					<div className='card-header card-header-danger card-header-icon'>
+						<div className='card-icon'>
+							<i className='material-icons'>pregnant_woman</i>
+						</div>
+						<p className='card-category'>Prenatal Records</p>
+						<h4 className='card-title'>{counts.prenatal_records}</h4>
+					</div>
+					<div className='card-footer'>
+						<div className='stats'>
+							<i className='material-icons'>local_offer</i> Total
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className='col-lg-3 col-md-6 col-sm-12'>
+				<div className='card card-stats'>
+					<div className='card-header card-header-info card-header-icon'>
+						<div className='card-icon'>
+							<i className='material-icons'>contact_page</i>
+						</div>
+						<p className='card-category'>Regular Records</p>
+						<h4 className='card-title'>{counts.regular_records}</h4>
+					</div>
+					<div className='card-footer'>
+						<div className='stats'>
+							<i className='material-icons'>update</i> Total
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className='col-lg-3 col-md-6 col-sm-12'>
 				<div className='card card-stats'>
 					<div className='card-header card-header-warning card-header-icon'>
 						<div className='card-icon'>
