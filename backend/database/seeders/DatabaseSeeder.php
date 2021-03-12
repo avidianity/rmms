79ed3babb7b175de4e16f773491b259acea60dc1
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2021-03-12 15:26:15 +0800

    Batch Update

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserSeeder::class);
-        $this->call(PatientSeeder::class);
-        $this->call(RecordSeeder::class);
+        // $this->call(PatientSeeder::class);
+        // $this->call(RecordSeeder::class);
     }
 }
